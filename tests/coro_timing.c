13f59428d9038e265d3088a440959f19622047d5
TheTechsTech <technoexpressnet@gmail.com>
2023-05-09 12:19:08 -0400

    add GHA CI, and performance timing test

---
+++ tests/coro_timing.c
@@ -0,0 +1,65 @@
+#define MINICORO_IMPL
+#define MCO_NO_DEBUG 1
+#include "minicoro.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+enum
+{
+    Iterations = 500000000
+};
+
+static int counter;
+
+void co_timingtest(mco_coro *co)
+{
+    for (;;)
+    {
+        counter++;
+        mco_yield(co);
+    }
+}
+
+void sub_timingtest()
+{
+    counter++;
+}
+
+int main()
+{
+    printf("context-switching timing test\n\n");
+    time_t start, end;
+    mco_coro *thread_y;
+    int i, t1, t2;
+
+    start = clock();
+    for (counter = 0, i = 0; i < Iterations; i++)
+    {
+        sub_timingtest();
+    }
+    end = clock();
+
+    t1 = (int)difftime(end, start);
+    printf("%2.3f seconds per  50 million subroutine calls (%d iterations)\n", (float)t1 / CLOCKS_PER_SEC, counter);
+
+    mco_desc desc = mco_desc_init(co_timingtest, 0);
+    desc.user_data = NULL;
+    mco_create(&thread_y, &desc);
+    mco_resume(thread_y);
+    start = clock();
+    for (counter = 0, i = 0; i < Iterations; i++)
+    {
+        mco_resume(thread_y);
+    }
+    end = clock();
+
+    mco_destroy(thread_y);
+
+    t2 = (int)difftime(end, start);
+    printf("%2.3f seconds per 100 million mco_resume calls (%d iterations)\n", (float)t2 / CLOCKS_PER_SEC, counter);
+
+    printf("mco_resume skew = %fx\n\n", (double)t2 / (double)t1);
+    return 0;
+}
